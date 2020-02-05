926f4bfaf9c7a3ab6f61002e75169641d030c66e
endless.pancake <endless.pancake4u@gmail.com>
2020-02-05 13:43:20 +0900

    Create new script

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+    return render_template("top.html")
+
+@app.route("/<name>", methods=["GET", "POST"])
+def namepage(name):
+    return render_template("trace2office365.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
